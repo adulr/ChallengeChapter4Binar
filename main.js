6f3ae1410490a252b4ecdb7e0e5c639f09a10b8e
adulr <abdul291298@gmail.com>
2022-08-13 01:39:35 +0700

    clear

---
+++ main.js
@@ -0,0 +1,82 @@
+class LetsPlayTheGame {
+
+    constructor() {
+        this.hands =  ['rock', 'paper','scissor'];
+        this.player = null;
+        this.playerElement = null;
+        this.comp = null;
+        this.compElement = null;
+        this.winner = null;
+        this.score = 0;
+        this.alreadySelected = false;
+        this.result = document.querySelector('.result');
+    }
+
+    match(element) {
+        if(!this.alreadySelected) {
+            this.playerElement = element;
+            this.alreadySelected = true;
+            this.playerElement.classList.add('border');
+            this.player = this.playerElement.id;
+            this.#randomCompHand();
+            this.#rules();
+            this.winner = this.#rules();
+            this.winner === 'PLAYER' ? this.score += 1 : this.score += 0;
+            console.log({ score: this.score });
+        } else {
+            alert('anda sudah memilih, klik refresh untuk main lagi');
+        }
+    }   
+
+    #randomCompHand() {
+        this.comp = this.hands[Math.floor(Math.random() * this.hands.length)];
+        this.compElement = document.querySelector(`#comp-${this.comp}`);
+        this.compElement.classList.add('border');
+      }
+
+    #rules() {
+        let msg;
+        if (this.player == this.comp) {
+          msg = 'DRAW';
+        } else {
+          if ((this.player == 'rock' && this.comp == 'scissor') || (this.player == 'scissor' && this.comp == 'paper') || (this.player == 'paper' && this.comp == 'rock')) {
+            msg = 'PLAYER WIN';
+          } else {
+            msg = 'COMP WIN';
+          }
+        }
+        this.result.innerHTML = msg;
+        
+        msg == 'DRAW' ? 
+        this.result.classList.add('msg', 'msg-draw') : 
+        this.result.classList.add('msg', 'msg-win');
+
+        if (msg === 'DRAW') {
+          return null;
+        }
+        if (msg === 'PLAYER WIN') {
+          return msg.split(' ')[0];
+        } else {
+          return msg.split(' ')[0];
+        }
+    }
+    refresh() {
+        if (this.alreadySelected) {
+          this.alreadySelected = false;
+          this.playerElement.classList.remove('border');
+          this.compElement.classList.remove('border');
+          this.player = null;
+          this.comp = null;
+          this.winner = null;
+          this.alreadySelected = false;
+          this.result.innerHTML = 'VS';
+          this.result.classList.remove('msg', 'msg-draw');
+          this.result.classList.remove('msg', 'msg-win');
+
+        } else {
+          return alert('Silahkan Pilih Tangan Untuk Bermain Lalu Menangkan!!');
+        }
+      }
+}
+
+const gameStart = new LetsPlayTheGame();
